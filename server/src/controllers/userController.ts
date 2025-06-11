e3a1c855982be959e55c00ee68ca9984cc1dcea8
proxycrome <e.v.ezeonwuka@gmail.com>
2025-06-11 09:53:17 +0100

    create user route post trigger

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -13,3 +13,28 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
       .json({ message: `Error retrieving users: ${error.message}` });
   }
 };
+
+export const createUser = async (req: Request, res: Response) => {
+  const {
+    username,
+    cognitoId,
+    profilePictureUrl = "i1.jpg",
+    teamId,
+  } = req.body;
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        cognitoId,
+        profilePictureUrl,
+        teamId,
+      },
+    });
+    res.status(200).json({ message: "User created successfully", newUser });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving users: ${error.message}` });
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -16,16 +16,16 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.send("This is the home route")
-})
+  res.send("This is the home route");
+});
 
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
@@ -34,7 +34,7 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
 // SERVER
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server is runnung on port ${port}`);
-})
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is runnung on port ${port}`);
+});
