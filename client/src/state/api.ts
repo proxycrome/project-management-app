5f99e5b97899bc5bb6237c834775a9acbb8ebb70
proxycrome <e.v.ezeonwuka@gmail.com>
2025-06-11 18:54:08 +0100

    fix cors error

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -81,7 +81,7 @@ export const api = createApi({
       const session = await fetchAuthSession();
       const { accessToken } = session.tokens ?? {};
       if (accessToken) {
-        headers.set("Anthorization", `Bearer ${accessToken}`);
+        headers.set("Authorization", `Bearer ${accessToken}`);
       }
       return headers;
     },
