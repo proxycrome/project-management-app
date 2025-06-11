ed2a68ef2e530cf22e8a6f7d74d9160fdb356032
proxycrome <e.v.ezeonwuka@gmail.com>
2025-06-11 16:41:06 +0100

    listen to default host

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,6 +35,6 @@ app.use("/teams", teamRoutes);
 
 // SERVER
 const port = Number(process.env.PORT) || 3001;
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server is runnung on port ${port}`);
 });
